5de923bfafebda5916fb0213a7f042738217bcff
Himnish Jain <67916778+Himnish@users.noreply.github.com>
2023-07-23 17:58:45 +0530

    Create 345.py
    Given a string s, reverse only all the vowels in the string and return it.
    The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.

---
+++ Easy/345.py
@@ -0,0 +1,39 @@
+# Reverse Vowels of a String
+
+# Given a string s, reverse only all the vowels in the string and return it.
+
+# The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+# Example 1:
+
+# Input: s = "hello"
+# Output: "holle"
+# Example 2:
+
+# Input: s = "leetcode"
+# Output: "leotcede"
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 3 * 105
+# s consist of printable ASCII characters.
+
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        vowels = "aeiouAEIOU"
+        sorted = []
+        ret = ""
+        for ch in s:
+            if ch in vowels:
+                sorted.append(ch)
+        for ch in s:
+            if ch in vowels:
+                ret += sorted.pop()
+            else:
+                ret += ch
+        return ret
