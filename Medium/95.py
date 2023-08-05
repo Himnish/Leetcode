f089e5150d88d003f5b4d608a6b3420e02f7346b
Himnish Jain <67916778+Himnish@users.noreply.github.com>
2023-08-05 07:24:52 +0530

    Create 95.py
    Given an integer n, return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n. Return the answer in any order.

---
+++ Medium/95.py
@@ -0,0 +1,37 @@
+# Unique Binary Search Trees II
+# Given an integer n, return all the structurally unique BST's (binary search trees), which has exactly n nodes of unique values from 1 to n. Return the answer in any order.
+
+# Example 1:
+# Input: n = 3
+# Output: [[1,null,2,null,3],[1,null,3,2],[2,1,3],[3,1,null,null,2],[3,2,null,1]]
+
+# Example 2:
+# Input: n = 1
+# Output: [[1]]
+
+# Constraints:
+# 1 <= n <= 8
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def generateTrees(self, n):
+        """
+        :type n: int
+        :rtype: List[TreeNode]
+        """
+        def generate(first, last):
+            trees = []
+            for root in range(first, last+1):
+                for left in generate(first, root-1):
+                    for right in generate(root+1, last):
+                        node = TreeNode(root)
+                        node.left = left
+                        node.right = right
+                        trees.append(node)
+            return trees or [None]
+        return generate(1, n)
